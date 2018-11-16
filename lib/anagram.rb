ad537ecd3e02b2751cbe25f29fdcb4d04136f12a
Ngan Nguyen <ngancamnguyen@gmail.com>
2018-11-16 11:17:57 -0800

    add method to validate both input are words and are anagrams

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,4 +1,18 @@
-def anagram_match(word_one, word_two)
+def word_has_vowel(word)
+  vowels = ['a','e','i','o','u']
+  word = word.split('')
+  word.any? {|letter| vowels.include?(letter)}
+end
+
+def these_are_words(word_one, word_two)
+ if (word_has_vowel(word_one) & word_has_vowel(word_two))
+   return true
+ else
+   return false
+ end
+end
+
+def words_match(word_one, word_two)
   word_one = word_one.split('')
   word_two = word_two.split('')
   if word_one.sort == word_two.sort
@@ -8,8 +22,13 @@ def anagram_match(word_one, word_two)
   end
 end
 
-def word_has_vowel(word)
-  vowels = ['a','e','i','o','u']
-  word = word.split('')
-  vowels.any? { |vowel| word.include?(vowel) }
+
+def both_words_are_anagram(word_one, word_two)
+  if these_are_words(word_one, word_two) & words_match(word_one, word_two)
+    return "is an anagram"
+  else
+    puts "#{word_one} is a word? #{word_has_vowel(word_one) }"
+    puts "#{word_two} is a word? #{word_has_vowel(word_two) }"
+    puts "Please enter only words!"
+  end
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -1,14 +1,27 @@
 require('anagram')
 require('rspec')
 
-describe('#anagram_match') do
-    it('returns true if the words contains the same letter') do
-    expect(anagram_match('ruby','bury')).to(eq(true))
-  end
-end
 
 describe('#word_has_vowel') do
     it('returns true if the word contain a vowel') do
     expect(word_has_vowel('hat')).to(eq(true))
+  end
+end
+
+describe('#these_are_words') do
+    it('returns true if both inputs are words') do
+    expect(these_are_words('ruby','bury')).to(eq(true))
+  end
+end
+
+describe('#words_match') do
+    it('returns true if the words contains the same letter') do
+    expect(words_match('ruby','bury')).to(eq(true))
+  end
+end
+
+describe('#both_words_are_anagram') do
+    it('returns true if the words contains the same letter') do
+    expect(both_words_are_anagram('sally','allys')).to(eq('is an anagram'))
   end
 end
